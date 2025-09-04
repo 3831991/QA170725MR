a66418a41fb8d4126daf3c8827b65090c0e5a7fb
Your Name <you@example.com>
2025-09-04 14:16:10 +0300

    added

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,3 +1,8 @@
 function welcomeMessage() {
     alert("ברוכים הבאים");
 }
+
+function welcome2() {
+    let userName = document.getElementById("userName").value;
+    alert("ברוך הבא " + userName);
+}
