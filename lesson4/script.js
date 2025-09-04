f4cd6f97f2f4d2ff7bab2d17c0b95d92920c0fd8
Your Name <you@example.com>
2025-09-04 15:59:59 +0300

    added

--- lesson4/script.js
+++ lesson4/script.js
@@ -6,3 +6,14 @@ function welcome2() {
     let userName = document.getElementById("userName").value;
     alert("ברוך הבא " + userName);
 }
+
+function multi50() {
+    let n = document.getElementById("num1").value;
+    alert(n * 50);
+}
+
+function multi() {
+    let n1 = document.getElementById("num2").value;
+    let n2 = document.getElementById("num3").value;
+    alert(n1 * n2);
+}
